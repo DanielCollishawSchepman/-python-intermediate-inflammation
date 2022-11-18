7e664b82c919868a03ee4d2dd81939aca792c82d
Daniel Collishaw-Schepman <daniel.collishaw-schepman@ukaea.uk>
2022-11-18 09:40:20 +0000

    Install and run isort and black

--- inflammation-analysis.py
+++ inflammation-analysis.py
@@ -3,7 +3,7 @@
 
 import argparse
 
-from inflammation import models, views, serializers
+from inflammation import models, serializers, views
 
 
 def main(args):
@@ -19,33 +19,35 @@ def main(args):
 
     for filename in infiles:
 
-        if args.view == 'visualize':
+        if args.view == "visualize":
             inflammation_data = models.load_csv(filename)
             view_data = {
-                'average': models.daily_mean(inflammation_data),
-                'max': models.daily_max(inflammation_data),
-                'min': models.daily_min(inflammation_data),
+                "average": models.daily_mean(inflammation_data),
+                "max": models.daily_max(inflammation_data),
+                "min": models.daily_min(inflammation_data),
             }
 
             views.visualize(view_data)
 
-        elif args.view == 'record':
+        elif args.view == "record":
             inflammation_data = models.load_csv(filename)
             patient_data = inflammation_data[args.patient]
-            observations = [models.Observation(day, value) for day, value in enumerate(patient_data)]
-            patient = models.Patient('UNKNOWN', observations)
+            observations = [
+                models.Observation(day, value) for day, value in enumerate(patient_data)
+            ]
+            patient = models.Patient("UNKNOWN", observations)
 
             views.display_patient_record(patient)
 
-        elif args.view == 'list-patients':
+        elif args.view == "list-patients":
             if args.serializer == "json":
                 records = serializers.PatientJSONSerializer.load(filename)
             elif args.serializer == "csv":
                 records = serializers.PatientCSVSerializer.load(filename)
 
             views.display_patients_list(records)
-        
-        elif args.view == 'new-patient':
+
+        elif args.view == "new-patient":
             if args.serializer == "json":
                 records = serializers.PatientJSONSerializer.load(filename)
             elif args.serializer == "csv":
@@ -59,8 +61,7 @@ def main(args):
                 serializers.PatientCSVSerializer.save(records, filename)
             views.display_patients_list(records)
 
-
-        elif args.view == 'new-observation':
+        elif args.view == "new-observation":
             if args.serializer == "json":
                 records = serializers.PatientJSONSerializer.load(filename)
             elif args.serializer == "csv":
@@ -76,47 +77,51 @@ def main(args):
 
             if args.serializer == "json":
                 serializers.PatientJSONSerializer.save(records, filename)
-                
+
             elif args.serializer == "csv":
                 serializers.PatientCSVSerializer.save(records, filename)
-            
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-        description='A basic patient inflammation data management system')
+        description="A basic patient inflammation data management system"
+    )
 
     parser.add_argument(
-        'infiles',
-        nargs='+',
-        help='Input CSV(s) containing inflammation series for each patient'
-        )
+        "infiles",
+        nargs="+",
+        help="Input CSV(s) containing inflammation series for each patient",
+    )
 
     parser.add_argument(
-        '--view',
-        default='visualize',
-        choices=['visualize', 'record', 'list-patients', 'new-patient', 'new-observation'],
-        help='Which view should be used?')
+        "--view",
+        default="visualize",
+        choices=[
+            "visualize",
+            "record",
+            "list-patients",
+            "new-patient",
+            "new-observation",
+        ],
+        help="Which view should be used?",
+    )
 
     parser.add_argument(
-        '--patient',
-        type=int,
-        default=0,
-        help='Which patient should be displayed?')
+        "--patient", type=int, default=0, help="Which patient should be displayed?"
+    )
+
+    parser.add_argument("--patientname", help="Which patient should be displayed?")
 
     parser.add_argument(
-        '--patientname',
-        help='Which patient should be displayed?')
+        "--observation", help="What is the value of the observation to be added?"
+    )
 
     parser.add_argument(
-        '--observation',
-        help='What is the value of the observation to be added?')
-
-    parser.add_argument(
-        '--serializer',
-        default='json',
-        choices=['json', 'csv'],
-        help='What serializer should be used?')
+        "--serializer",
+        default="json",
+        choices=["json", "csv"],
+        help="What serializer should be used?",
+    )
 
     args = parser.parse_args()
 
